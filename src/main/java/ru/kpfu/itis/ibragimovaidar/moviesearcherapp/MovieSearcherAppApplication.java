299b768217a7ac564e619702048366ba6b11bb45
ibragimovaidar <ibragimovaidarp@gmail.com>
2022-05-05 12:59:35 +0300

    base structure

---
+++ src/main/java/ru/kpfu/itis/ibragimovaidar/moviesearcherapp/MovieSearcherAppApplication.java
@@ -0,0 +1,13 @@
+package ru.kpfu.itis.ibragimovaidar.moviesearcherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSearcherAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieSearcherAppApplication.class, args);
+    }
+
+}
